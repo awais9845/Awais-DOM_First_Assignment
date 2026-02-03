fe87ddafa5d74744ac7e17eeffd231edd2916b77
awais9845 <waqarkhattak844@gmail.com>
2026-02-02 21:33:15 -0800

    add the logic the input field will empty automatically when click on submit btn

--- script.js
+++ script.js
@@ -11,14 +11,14 @@ let para1 = document.getElementById("para1");
 function changeText(){
     para1.innerText = "Paragraph text changed!";
 }
-
+// task 3
 
 let body = document.body;
 
 function changeBackground(){
     body.style.backgroundColor = "lightblue";
 }
-
+// task 4
 function showAlert(){
     alert("DOM is Working");
 }
@@ -29,4 +29,5 @@ let yourName = document.getElementById("yourName");
 let showName = document.getElementById("showName");
 function submitbtn(){
     showName.innerText = yourName.value;
+    yourName.value = "";
 }
